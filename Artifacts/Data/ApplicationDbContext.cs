56635b29de4ee496c0d0c63f04d81bebea7abaf9
Vincent <meijer.vincent@gmail.com>
2017-02-26 12:06:09 -0500

    Migrate ApplicationDbContext from previous project (with error on SaveChangesAsync)

--- Artifacts/Data/ApplicationDbContext.cs
+++ Artifacts/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Artifacts.Models;
+using Artifacts.Models.Entities;
 
 namespace Artifacts.Data
 {
@@ -22,5 +23,31 @@ namespace Artifacts.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public override async Task<int> SaveChangesAsync()
+        {
+            AddTimestamps();
+            return await base.SaveChangesAsync();
+        }
+
+        private void AddTimestamps()
+        {
+            var entities = ChangeTracker.Entries().Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+            var now = DateTime.Now;
+
+            foreach (var entity in entities)
+            {
+                if (entity.State == EntityState.Added)
+                {
+                    ((BaseEntity)entity.Entity).Created = now;
+                }
+
+                ((BaseEntity)entity.Entity).Edited = now;
+            }
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<BlogPostImage> BlogPostImages { get; set; }
+        public DbSet<BlogPostTag> Tags { get; set; }
     }
 }
